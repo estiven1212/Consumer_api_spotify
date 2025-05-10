9bdb888510762189b41e275ae8eab6328aac6371
Frasmo <hermessoto01@gmai.com>
2025-05-09 19:58:17 -0500

    cambio de color del logo

--- src/auth/components/HomePage.jsx
+++ src/auth/components/HomePage.jsx
@@ -9,10 +9,7 @@ const HomePage = () => {
       <video src={videoBg} autoPlay loop muted className="video-bg" />
       <div className="content">
         <Navbar />
-        <p>Quieres escuchas tu musica en cualquier lugra
-          Unete a nuestra nueva app, que permite reproducir sus canciones
-          favoritas sin ningún costo. ¡Te esperamos!
-        </p>
+        
         <h1>El equipo dinamita te muestra tu musica preferida</h1>
       </div>
     </div>
